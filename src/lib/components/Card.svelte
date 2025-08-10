8a224c735c349020a741dbcd9d44767ce5a805d1
AnotherPillow <contact@pillow.rocks>
2025-08-10 15:33:48 +0100

    Initial Commit

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    let { url, img, title, small = undefined } = $props();
+</script>
+
+<a
+    class="w-72 h-72 bg-zinc-800 rounded-md border-zinc-700 border-6 filter hover:brightness-80 cursor-pointer p-4 relative overflow-hidden"
+    href={url}
+    target="_blank"
+>
+    <div
+        class="absolute inset-0 hover:mt-[-16px] bg-no-repeat bg-cover bg-top transition-all"
+        style={`background-image: url(${img});`}
+    ></div>
+
+    <div
+        class="bg-zinc-700 z-20 absolute left-0 bottom-0 right-0 h-20 flex justify-center flex-col"
+    >
+        <h2 class="text-xl font-bold text-center">{title}</h2>
+        {#if small}
+            <p class="text-center text-xm text-neutral-400">{small}</p>
+        {/if}
+    </div>
+</a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Card from "$lib/components/Card.svelte";
+
+</script>
+<main class="relative min-w-screen min-h-screen font-[Lato]">
+    <div class="absolute inset-0 bg-gradient-to-br from-pink-800 via-fuchsia-900 to-purple-950"></div>
+    <div class="absolute inset-0 bg-black/70"></div>
+
+    
+    <div class="relative z-10 text-white flex min-w-screen min-h-screen justify-center items-center flex-col">
+        <a href="/" target="_self" class="hover:text-decoration-line filter hover:brightness-80">
+            <h1 class="text-6xl font-bold mb-12">alts.<span class="text-rose-300">best</span></h1>
+        </a>
+
+        <div class="w-240 h-80 flex justify-center items-center gap-8">
+            <Card 
+                url="https://www.minecraft.net/store/minecraft-java-bedrock-edition-pc"
+                img="https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/PDP-Hero_OV-Standard_16x9.jpg"
+                title="Minecraft Java & Bedrock"
+            />
+            <Card 
+                url="https://feverup.com/m/282847"
+                img="https://minecraft.wiki/images/Minecraft_Experience_Key_Art.jpg"
+                small="From £22"
+                title="Minecraft Experience Cape"
+            />
+            <!-- yes i'm aware it's on variety.com if you have the original source/one from xbox just tell me -->
+            <Card 
+                url="https://www.xbox.com/en-US/xbox-game-pass"
+                img="https://variety.com/wp-content/uploads/2021/06/Game-Pass.jpg" 
+                small="From $1"
+                title="Xbox Game Pass"
+            />
+        </div>
+    </div>
+</main>
